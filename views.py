de1ce9d80c4bf2355392e4b3e0d0a4e48319ce98
abinash-987 <abinashmallick987@gmail.com>
2025-08-21 15:59:35 +0530

    Add files via upload
    Third

---
+++ views.py
@@ -0,0 +1,11 @@
+from rest_framework import viewsets
+from rest_framework import filters
+from .models import Todo
+from .serializers import TodoSerializer
+
+
+class TodoViewSet(viewsets.ModelViewSet):
+    queryset = Todo.objects.all().order_by("-created_at")
+    serializer_class = TodoSerializer
+    filter_backends = [filters.SearchFilter]
+    search_fields = ["title", "description"]
